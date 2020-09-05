1b03eacdafdbed02c6e96e0099899f21b9735962
Daniel Gnaegi <mail@dgnaegi.ch>
2020-09-05 15:55:33 +0200

    Updates

---
+++ precache-manifest.990db46b9cb8372666dbdad1c5770970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53512bbb447ae9498b0ba6d523ca476c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56ab09bd52cdb8f7cae3",
+    "url": "/static/js/2.2a7f8ec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a7f8ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534622934032244c27f7",
+    "url": "/static/js/main.ff6cde85.chunk.js"
+  },
+  {
+    "revision": "41045d4f5fd38b909f6c",
+    "url": "/static/js/runtime-main.a2ef797a.js"
+  }
+]);
